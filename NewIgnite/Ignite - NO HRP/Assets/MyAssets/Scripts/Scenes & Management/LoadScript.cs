334c241919e6c11a6d88b01cffd6cecb1af66321
BinarySandia04 <aranseraroig@gmail.com>
2018-08-06 19:45:11 +0200

    Jod** si he hecho cambios...

---
+++ NewIgnite/Ignite - NO HRP/Assets/MyAssets/Scripts/Scenes & Management/LoadScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScript : MonoBehaviour {
+
+    public GameObject loadScreen;
+
+    public void LoadLevel(string name)
+    {
+        loadScreen.gameObject.SetActive(true);
+
+        StartCoroutine(LoadAsyncronously(name));
+    }
+
+    private void Start()
+    {
+        loadScreen.gameObject.SetActive(false);
+    }
+
+    IEnumerator LoadAsyncronously (string name)
+    {
+        Color c = new Color(0.8f, 0.8f, 0.8f, 0);
+        for(float a = 0; a < 1; a+=0.01f)
+        {
+            c.a = a;
+            loadScreen.GetComponent<Image>().color = c;
+
+            yield return new WaitForSeconds(0.005f);
+        }
+        AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            loadScreen.transform.Find("Slider").gameObject.GetComponent<Slider>().value = progress;
+
+            yield return null;
+        }
+    }
+
+}
---
+++ NewIgnite/Ignite - NO HRP/Assets/MyAssets/Scripts/Scenes & Management/UserAccountManagement.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using DatabaseControl;
+
+public class UserAccountManagement : MonoBehaviour {
+
+    public static UserAccountManagement instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(instance);
+    }
+    
+    public static string playerUsername { get; protected set; }
+    public static string playerPassword { get; protected set; }
+
+    public static bool isLoggedIn { get; protected set; }
+
+    public static string LoggedIn_Data { get; protected set; }
+
+    public void LogOut()
+    {
+        playerPassword = "";
+        playerUsername = "";
+
+        isLoggedIn = false;
+    }
+
+    public void LogIn(string username, string password)
+    {
+        playerUsername = username;
+        playerPassword = password;
+
+        isLoggedIn = true;
+    }
+
+    IEnumerator GetData()
+    {
+        IEnumerator e = DCF.GetUserData(playerUsername, playerPassword); // << Send request to get the player's data string. Provides the username and password
+        while (e.MoveNext())
+        {
+            yield return e.Current;
+        }
+        string response = e.Current as string; // << The returned string from the request
+
+        if (response == "Error")
+        {
+            Debug.Log("Error");
+        }
+        else
+        {
+            Debug.Log(response);
+            LoggedIn_Data = response;
+        }
+    }
+    IEnumerator SendData(string data)
+    {
+        IEnumerator e = DCF.SetUserData(playerUsername, playerPassword, data); // << Send request to set the player's data string. Provides the username, password and new data string
+        while (e.MoveNext())
+        {
+            yield return e.Current;
+        }
+        string response = e.Current as string; // << The returned string from the request
+
+        if (response == "Success")
+        {
+            Debug.Log("Data enviada!");
+
+        }
+    }
+}
